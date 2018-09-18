1cd01f783468dfea925712aa7a1c1b30a0940b44
KurnosovNikita <Kurnosov-nikita@mail.ru>
2018-09-18 09:40:42 +0300

    init

--- src/helpers.js
+++ src/helpers.js
@@ -5,12 +5,12 @@ function formatResponse(err, data, message) {
         result: data,
         links: [
            {
-                href: 'https://mmg-score.herokuapp.com/',
+                href: 'https://kurnosov-nikita-bet.herokuapp.com/',
                 rel: 'get full score',
                 method: 'GET',
            },
            {
-               href: 'https://mmg-score.herokuapp.com/',
+               href: 'https://kurnosov-nikita-bet.herokuapp.com/',
                rel: 'save single score result',
                method: 'POST',
                body: 'username=Nikita&email=randomemail@box.com&score=10',
